23d9a27e14c8b951365cd75c45d91ef93e768996
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-05-20 23:54:06 -0500

    added week 1 assignment

---
+++ week1/math/app.js
@@ -0,0 +1,6 @@
+const  mathObj = require('./math')
+
+console.log (mathObj.sum(10,2));
+console.log (mathObj.subtract(10,5));
+console.log (mathObj.divide(8,2));
+console.log (mathObj.multiply(5,2));
---
+++ week1/math/math.js
@@ -0,0 +1,23 @@
+const sum = (a, b) => {
+    return a+b
+};
+
+
+const subtract = (c, d) => {
+    return c - d
+};
+
+const divide = (e, g)=> {
+    return e/g
+};
+
+const multiply = (s, t) =>{
+    return s*t
+};
+
+module.exports = {
+    sum: sum,
+    subtract: subtract,
+    divide: divide,
+    multiply: multiply
+};
